64528628fbaca0abf2314af2259d35f3821cf3ae
Aliaksiej Ramanchuk <alesha.rom@gmail.com>
2023-03-07 03:05:35 +0300

    Complete part 4, correct .content__button class, add styles for section names

--- src/components/Content/Introduction.vue
+++ src/components/Content/Introduction.vue
@@ -2,18 +2,18 @@
 import {ref} from 'vue'
 import ContentButton from './ContentButton.vue'
 import '../styles/introduction.scss'
+import '../styles/content__button.scss'
 import TextContent from './TextContent.vue'
 const introductionButtonText = ref("shop collection")
 </script>
 
 <template>
     <div class="introduction">
-    <TextContent>
-      <p class="introduction__pompeoPottery"> Pompeo Pottery </p>
-      <h1 class = "introduction__textContent-bold"> Unique Porcelain </h1>
-    </TextContent>
+        <TextContent>
+          <p class="introduction__pompeoPottery"> Pompeo Pottery </p>
+          <h1 class = "introduction__textContent-bold"> Unique Porcelain </h1>
+        </TextContent>
         <div class=" introduction__pot-image"></div>
         <ContentButton :props = "introductionButtonText"/>
     </div>
 </template>
-
---
+++ src/components/Content/PotteryCollectionItem.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+const props = defineProps({
+    imgSrc: String,
+    itemName: String,
+    price: Number,
+})
+</script>
+
+<template>
+    <figure class="collection__content__items__item">
+        <img :src="imgSrc"/>
+        <figcaption>
+            <p class="collection__content__items__item_name">{{itemName}}</p>
+            <p class="collection__content__items__item_price">$ {{price?.toFixed(2)}} USD</p>
+        </figcaption>
+    </figure>
+</template>
