f82c6baf5a6dacba0f03dd9357fcf87887e78146
Ilya <iltihomsorok@gmail.com>
2023-03-07 15:53:27 +0300

    correct introduction: added text content, correct styles for content__button class, add styles for text content

--- src/components/Content/Introduction.vue
+++ src/components/Content/Introduction.vue
@@ -10,10 +10,17 @@ const introductionButtonText = ref("shop collection")
 <template>
     <div class="introduction">
         <TextContent>
+        <div class="introduction__textContent_wrapper">
           <p class="introduction__pompeoPottery"> Pompeo Pottery </p>
-          <h1 class = "introduction__textContent-bold"> Unique Porcelain </h1>
+          <h1 class = "introduction__textContent_bold">
+          Unique Porcelain 
+            <span class="introduction__logoText">&</span></h1>
+       <h1 class = "introduction__textContent_bold" > &#13; Stone Collection</h1>
+        </div>
         </TextContent>
-        <div class=" introduction__pot-image"></div>
-        <ContentButton :props = "introductionButtonText"/>
+        <div class=" introduction__pot_image"></div>
+        <div class="content__button_wrapper">
+            <ContentButton :props = "introductionButtonText"/>
+        </div>
     </div>
 </template>
