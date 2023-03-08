426a6ed5b550140774ca68caeb97f6ebe5daf3b1
Aliaksiej Ramanchuk <alesha.rom@gmail.com>
2023-03-09 02:42:24 +0300

    Complete footer

---
+++ src/components/Footer/RowItem.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const props = defineProps({
+    imgSrc: String,
+    title: String,
+    info: String,
+})
+</script>
+
+<template>
+    <figure class="footer__content__row__item">
+        <img :src="imgSrc"/>
+        <figcaption>
+            <p class="footer__content__row__item_title">{{title}}</p>
+            <p class="footer__content__row__item_info">{{info}}</p>
+        </figcaption>
+    </figure>
+</template>
+
