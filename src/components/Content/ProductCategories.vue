99c420e2ff7afe910cba75f361d9da258fff0888
Ilya <iltihomsorok@gmail.com>
2023-03-09 21:46:46 +0300

    add Gold & Black Pottey, fixed introduction styles

--- src/components/Content/ProductCategories.vue
+++ src/components/Content/ProductCategories.vue
@@ -1,21 +1,26 @@
 <script setup lang="ts">
     import {ref} from 'vue';
-    import './'
+    import '../styles/categories.scss'
+
 </script>
 
 <template id="">
     <div class="categories">
+        <div class="categories__image_wrapper">
+            <img class="categories__image" src="https://assets.website-files.com/5ba96977bf120e1b45986c06/5bb9668024337c19082edcaf_orientovase.png" >
+        </div>
         <TextContent>
             <div class="categories__textContent_wrapper">
-               <h1 class categories__textContent_bold>
+               <h1 class="categories__textContent_bold">
                     Gold & Black Pottery
                </h1> 
                <p class="categories__textContent"> 
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit sed do eiusmod tempor
                     incididunt ut labore dolore aua. Ut enim ad minim veniam, quis nostrud exercitationulco
                     laboris nisi ut aliquip ex ea commodo consequatuisaute.
+                    <span class="categories__viewDetails">View Details</span>
                </p>
             </div>
         </TextContent>
-    </div> 
+     </div> 
 </template>
