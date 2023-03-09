5d0609e5cc3787d3fe5108f5eb4393b61c8a4a56
Ilya <iltihomsorok@gmail.com>
2023-03-09 18:57:53 +0300

    initialized Gold&Black Pottery component

---
+++ src/components/Content/ProductCategories.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+    import {ref} from 'vue';
+    import './'
+</script>
+
+<template id="">
+    <div class="categories">
+        <TextContent>
+            <div class="categories__textContent_wrapper">
+               <h1 class categories__textContent_bold>
+                    Gold & Black Pottery
+               </h1> 
+               <p class="categories__textContent"> 
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit sed do eiusmod tempor
+                    incididunt ut labore dolore aua. Ut enim ad minim veniam, quis nostrud exercitationulco
+                    laboris nisi ut aliquip ex ea commodo consequatuisaute.
+               </p>
+            </div>
+        </TextContent>
+    </div> 
+</template>
