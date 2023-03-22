e956661ecb597523b0a590aecd364f95cba951d5
dalvhikki <v.goroshchenja@gmail.com>
2023-03-23 01:39:49 +0300

    changes in layout of categories(add props and components for them)

---
+++ src/components/Content/CategoryItem.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const props = defineProps({
+    imgSrc: String,
+    itemName: String,
+    color: String,
+})
+</script>
+
+<template>
+    <div class="VMP_item" v-bind:style="{backgroundColor:color}">
+        <img :src="imgSrc"/>
+        <p>{{itemName}}</p>
+    </div>
+</template>
